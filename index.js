9c9fa145fd5145c24a07841d74f475cd93de0808
pancham kumar <panchamku8873@gmail.com>
2022-06-16 12:29:04 +0530

    setup for basic user schema with pass encryption done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const {connect }=require("./src/config/database")
 const apiRouter = require("./src/routes/index");
+const User = require("./src/model/user");
 const app = express();
 
 app.use("/api",apiRouter);
@@ -7,6 +9,8 @@ app.get('/',(req,res)=>{
     res.send("Helo world");
 })
 
-app.listen(3000,()=>{
+app.listen(3000,async ()=>{
+     await connect();
+    console.log("mono db connected successfully");
     console.log("server started successfully");
 })
--- src/controllers/helpController.js
+++ src/controllers/helpController.js
@@ -1,6 +1,7 @@
 const helpDetails = (req,res)=>{
      return res.status(200).send({
         message:"Succesfully hitting the help API",
+        success:true,
         data:{
             contact:"+91XXXXXXXX"
         }
